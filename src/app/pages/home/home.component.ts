47fe02c625af1023a6dbcec8497a23ba387f5258
Subhi YD <subhy809fr@gmail.com>
2023-09-20 22:54:44 +0200

    v1.0.0 Mise en place du starter code and Compréhension et définition des données, create models OlympicCountry, Participation

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,3 +1,4 @@
+import { OlympicCountry } from './../../core/models/Olympic';
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
@@ -8,11 +9,16 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  public olympics$: Observable<any> = of(null);
+  errorMessage!: string; 
+  public olympics$: Observable<OlympicCountry[]> = of([]);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+    if(!this.olympics$) {
+     this.errorMessage = 'Une erreur s\'est produite lors du chargement des données.';
+     console.log(this.errorMessage)
+    }
   }
 }
