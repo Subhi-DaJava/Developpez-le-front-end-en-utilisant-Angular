25330ea269a5daa7d90d2d2f0a9fdf7d4115542a
Subhi YD <subhy809fr@gmail.com>
2023-09-22 18:00:11 +0200

    v1.1.0 install npm package for char.js, update HomeComponent, template, style SCSS, create methods, without Responsive step 3 & 4

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { OlympicCountry } from './../../core/models/Olympic';
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 import { OlympicService } from 'src/app/core/services/olympic.service';
 
 @Component({
@@ -9,16 +11,73 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  errorMessage!: string; 
-  public olympics$: Observable<OlympicCountry[]> = of([]);
+  errorMessage!: string;
+  olympics$!: OlympicCountry[];
 
-  constructor(private olympicService: OlympicService) {}
+  countries: string[] = [];
+  totalMedals: number[] = [];
+  totalParticipation: number[] = [];
+  totolCounterNumberOfOlympics!: number;
+
+  constructor(private olympicService: OlympicService, private router: Router) { }
 
   ngOnInit(): void {
-    this.olympics$ = this.olympicService.getOlympics();
-    if(!this.olympics$) {
-     this.errorMessage = 'Une erreur s\'est produite lors du chargement des données.';
-     console.log(this.errorMessage)
-    }
+    this.olympicService.getOlympics().subscribe({
+      next: olympics => {
+        this.olympics$ = olympics;
+
+        if (this.olympics$ && this.olympics$.length > 0) {
+
+          this.countries = this.olympics$.map(o => o.country);
+          this.totalParticipation = this.olympics$.map(o => o.participations.length);
+          this.totalMedals = this.olympics$.map(o => this.getTotalMedals(o));
+          this.totolCounterNumberOfOlympics = this.countTotalOlympics(this.olympics$);
+          this.renderCharJs();
+        }
+      }, error: err => {
+        this.errorMessage = err.error;
+        console.log('An error occurred while loading data.');
+      }
+
+    });
+  }
+
+  private getTotalMedals(country: OlympicCountry): number {
+    return country.participations.map(participation => participation.medalsCount).reduce((medals, total) => { return medals + total; }, 0);
+  }
+
+  private countTotalOlympics(countries: OlympicCountry[]): number {
+    const uniqueYears = new Set<number>(
+      countries
+        .map(country =>
+          country.participations.map(participation => participation.year)
+        )
+        .flat()
+    );
+
+    return uniqueYears.size;
+  }
+
+  renderCharJs() {
+    new Chart('myChartJs', {
+      type: 'pie',
+      data: {
+        labels: this.countries,
+        datasets: [{
+          label: '🥇 Total Medals',
+          data: this.totalMedals,
+
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+        },
+        onClick: (e) => {
+          console.log(e);
+          this.router.navigateByUrl(`coutries/1`).then();
+        }
+      }
+    });
   }
 }
